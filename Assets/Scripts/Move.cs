56d8dbdc7571134bf0ea4a795286bc7a0f0ac66a
ilektrym <ilektr19@mail.ru>
2024-10-17 20:13:00 +0300

    first commit

---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.MLAgents;
+using Unity.MLAgents.Actuators;
+using Unity.MLAgents.Sensors;
+using UnityEngine;
+
+public class Move : Agent
+{
+    [SerializeField] private Transform targetTransform;
+    [SerializeField] private Material winMaterial;
+    [SerializeField] private Material loseMaterial;
+    [SerializeField] private MeshRenderer planeMeshRederer;
+
+    public override void OnEpisodeBegin()
+    {
+        transform.localPosition = new Vector3(-15, 3f, 14);
+        targetTransform.localPosition = new Vector3(Random.Range(-8,16), 3f, -7);
+    }
+
+    private Rigidbody rb;
+
+    public override void Initialize()
+    {
+        rb  = GetComponent<Rigidbody>();
+    }
+
+    public override void CollectObservations(VectorSensor sensor)
+    {
+        sensor.AddObservation(transform.localPosition);
+        sensor.AddObservation(targetTransform.localPosition);
+    }
+
+    public override void Heuristic(in ActionBuffers actionsOut)
+    {
+        ActionSegment<float> ContinuousActions = actionsOut.ContinuousActions;
+
+        ContinuousActions[0] = Input.GetAxisRaw("Horizontal");
+        ContinuousActions[1] = Input.GetAxisRaw("Vertical");
+    }
+
+    public override void OnActionReceived(ActionBuffers actions)
+    {
+        float moveX = actions.ContinuousActions[0];
+        float moveZ = actions.ContinuousActions[1];
+
+        float speed = 20f;
+        /*
+        rb.MovePosition(transform.position + transform.forward * moveZ * speed * Time.deltaTime);
+        transform.Rotate(0f, moveX * speed, 0f, Space.Self);
+        */
+
+        transform.localPosition += new Vector3(moveX, 0, moveZ) * Time.deltaTime * speed;
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.TryGetComponent<Wall>(out Wall wall))
+        {
+            SetReward(-1f);
+            planeMeshRederer.material = loseMaterial;
+            EndEpisode();
+        }
+
+        if (other.TryGetComponent<Obstacle>(out Obstacle obstacle))
+        {
+            SetReward(-1f);
+            planeMeshRederer.material = loseMaterial;
+            EndEpisode();
+        }
+
+        if (other.TryGetComponent<Goal>(out Goal goal))
+        {
+            SetReward(+1f);
+            planeMeshRederer.material = winMaterial;
+            EndEpisode();
+        }
+    }
+}
