5125db8da2e82111b1466e3b656df85a3a7b62db
M1CAELAN <michaelklinkov@gmail.com>
2024-10-26 16:43:19 +0300

    I have aged, but I have trained him.

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -15,7 +15,7 @@ public class Move : Agent
 
     public override void OnEpisodeBegin()
     {
-        transform.localPosition = new Vector3(Random.Range(-18, 18), 3f, Random.Range(-18, 18));
+        transform.localPosition = new Vector3(Random.Range(-17, 17), 3f, Random.Range(-17, 17));
         targetTransform.localPosition = new Vector3(Random.Range(-18,18), 3f, Random.Range(-18, 18));
     }
 
@@ -45,17 +45,8 @@ public class Move : Agent
         float moveX = actions.ContinuousActions[0];
         float moveZ = actions.ContinuousActions[1];
 
-<<<<<<< HEAD
-       
         rb.MovePosition(transform.position + transform.forward * moveZ * speed * Time.deltaTime);
         transform.Rotate(0f, moveX * speed, 0f, Space.Self);
-      
-=======
-        
-        rb.MovePosition(transform.position + transform.forward * moveZ * speed * Time.deltaTime);
-        transform.Rotate(0f, moveX * speed, 0f, Space.Self);
-        
->>>>>>> 5ebd7f7b829e4ee136af673fdb2d43e6744e3680
         //transform.localPosition += new Vector3(moveX, 0, moveZ) * Time.deltaTime * speed;
 
     }
