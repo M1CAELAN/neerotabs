694dfdd6633c5daaf2dfdc5e0ef2eef8d5b5b323
M1CAELAN <michaelklinkov@gmail.com>
2024-12-11 23:23:33 +0300

    fix_menu

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -7,7 +7,7 @@ public class MainMenu : MonoBehaviour
 {
     public void Play()
     {
-        SceneManager.LoadScene("game");
+        SceneManager.LoadScene("SampleScene");
     }
 
     public void Exit()
